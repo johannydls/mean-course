59d7f8f15131d9e4442358f4b1c93a15cdc805e8
Johanny de Lucena Santos <johanny.santos@ccc.ufcg.edu.br>
2018-09-12 12:48:26 -0300

    Comit inicial com componente basico implementado

---
+++ src/app/posts/post-create/post-create.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-post-create',
+  templateUrl: './post-create.component.html'
+})
+
+export class PostCreateComponent {
+
+  enteredValue = '';
+  newPost = 'NO CONTENT';
+
+  //onAddPost(postInput: HTMLTextAreaElement) {
+  onAddPost() {
+    //console.dir(postInput);
+    this.newPost = this.enteredValue;
+  }
+}
